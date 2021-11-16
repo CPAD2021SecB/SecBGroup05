f7726c94b06474aa6f0d330bcda73e0a1ad56741
I524745 <shivani.donthi@sap.com>
2021-11-16 18:29:50 +0530

    Sign Up & Sign In

--- momento/lib/main.dart
+++ momento/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:momento/pages/SignUpPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +20,7 @@ class _MyAppState extends State<MyApp> {
   void signup() async {
     try {
       await firebaseAuth.createUserWithEmailAndPassword(
-          email: "devstackin1@gmail.com", password: "123456");
+          email: "testuser@gmail.com", password: "123456");
     } catch (e) {
       print(e);
     }
@@ -28,19 +29,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Momento"),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              signup();
-            },
-            child: Text("Signup"),
-          ),
-        ),
-      ),
+      home: SignUpPage(),
     );
   }
 }
